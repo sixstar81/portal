00952c56975924a422ea24ecc10430dbcecfab13
“sixtar81” <sixtar6@dbgroup.co.kr>
2025-04-15 09:51:57 +0900

    te

--- src/main/java/com/inc/smart/portal/document/domain/enums/TopMenu.java
+++ src/main/java/com/inc/smart/portal/document/domain/enums/TopMenu.java
@@ -17,9 +17,9 @@ public enum TopMenu {
         this.code = code;
         this.name = name;
     }
-    //return List<int>
+    
     public static List<Integer> topMenuCodeList() {
-        
+
         return Arrays.stream(values())
                 .map(TopMenu::getCode)
                 .collect(Collectors.toList());
